8120077b14651ae26ce25783e2307613e4943f18
Igor <tanovigor@gmail.com>
2021-07-04 21:47:03 +0300

    написание функционального метода

--- script.js
+++ script.js
@@ -2,38 +2,65 @@
 // LESSON_1
 let numberOfFilms;
 
-function start() {
-    numberOfFilms = prompt("Сколько фильмов вы уже смотрели?");
-    while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = prompt("Сколько фильмов вы уже смотрели?");
-    }
-}
-start();
+// function start() {
+//     numberOfFilms = prompt("Сколько фильмов вы уже смотрели?");
+//     while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
+//         numberOfFilms = prompt("Сколько фильмов вы уже смотрели?");
+//     }
+// }
+// start();
 let personalMovieDB = {};
 personalMovieDB.count = numberOfFilms;
 personalMovieDB.movies = {};
 personalMovieDB.actors = {};
 personalMovieDB.genres = [];
 personalMovieDB.privat = false;
-for (let i = 0; i < 2; i++) {
-    let a = prompt("Один из последних просмотренных фильмов?");
-    let b = prompt("На скольео его оцените?");
-    if (a == "" || b == "" || a.length > 50 || a === null || b == null) {
-        alert("не коректный ответ");
-        i--;
+// for (let i = 0; i < 2; i++) {
+//     let a = prompt("Один из последних просмотренных фильмов?");
+//     let b = prompt("На скольео его оцените?");
+//     if (a == "" || b == "" || a.length > 50 || a == null || b == null) {
+//         alert("не коректный ответ");
+//         i--;
+//     }
+
+//     personalMovieDB.movies[a] = b;
+// }
+
+// function showMyDB() {
+//     if (personalMovieDB.privat == false) {
+//         console.log(personalMovieDB);
+//     }
+// }
+
+// function writeYourGenres() {
+//     for (let i = 1; i < 4; i++) {
+//         personalMovieDB.genres[i - 1] = (prompt(`Ваш любимый жанр под номером ${i}`));
+//     }
+// }
+personalMovieDB.start = function () {
+    personalMovieDB.count = prompt("Сколько фильмов вы уже смотрели?");
+    while (personalMovieDB.count == "" || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+        personalMovieDB.count = prompt("Сколько фильмов вы уже смотрели?");
     }
-
-    personalMovieDB.movies[a] = b;
-}
-
-function showMyDB() {
+};
+personalMovieDB.toogleVisibleMyDB = function () {
     if (personalMovieDB.privat == false) {
-        console.log(personalMovieDB);
+        personalMovieDB.privat = true;
+    } else {
+        personalMovieDB.privat = false;
     }
-}
+};
 
-function writeYourGenres() {
+
+personalMovieDB.writeYourGenres = function () {
     for (let i = 1; i < 4; i++) {
-        personalMovieDB.genres[i - 1] = (prompt(`Ваш любимый жанр под номером ${i}`));
+        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+        while (personalMovieDB.genres[i - 1] == "" || personalMovieDB.genres[i - 1] == null) {
+            personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+        }
     }
-}
+    let f=0;
+    personalMovieDB.genres.forEach((item,i) =>
+        console.log(`Любимый жанр №${i+1}-это ${item}`));
+
+};
